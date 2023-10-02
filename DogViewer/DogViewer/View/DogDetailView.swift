7128dc8caf53cdf9c7aca9ae4a4c05eea524db45
Manajit Halder <manajit.halder@gmail.com>
2023-10-02 21:22:59 +0530

    adding launch screen

---
+++ DogViewer/DogViewer/View/DogDetailView.swift
@@ -0,0 +1,19 @@
+//
+//  DogDetailView.swift
+//  
+//  Created by Manajit Halder on 02/10/23 using Swift 5.0 on MacOS 13.4
+//  
+
+import SwiftUI
+
+struct DogDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DogDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DogDetailView()
+    }
+}
